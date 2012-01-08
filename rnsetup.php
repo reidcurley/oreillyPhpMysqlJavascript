85d4313f11800bdf5bbdbd02566441581b4fabdc
Reid Curley <rcurley@ruxtonpartners.com>
2012-01-08 10:44:06 -0500

    added rnsetup.php

---
+++ rnsetup.php
@@ -0,0 +1,18 @@
+<?php // rnsetup.php
+include_once 'rnfunctions.php';
+echo '<h3>Setting up</h3>';
+
+createTable('rnmembers', 'user VARCHAR(16), pass VARCHAR(16),
+	INDEX(user(6))');
+
+createTable('rnmessages',
+	'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	auth VARCHAR(16), recip VARCHAR(16), pm CHAR(1),
+	INDEX(auth(6)), INDEX(recip(6))');
+
+creatTable('rnfriends', 'user VARCHAR(16), friend VARCHAR(16),
+	INDEX(user(6)), INDEX(friend(6))');
+
+creatTable('rnprofiles', 'user VARCHAR(16), text VARCHAR(4096),
+	INDEX(user(6))');
+?>
